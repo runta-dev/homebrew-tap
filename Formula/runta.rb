4036f8660dbccba0659a4781d9691bf75c915868
Xi Chen <chenxi.edu1997@gmail.com>
2026-06-29 23:31:57 +0000

    release 0.0.5

--- Formula/runta.rb
+++ Formula/runta.rb
@@ -1,12 +1,12 @@
 class Runta < Formula
   desc "CLI for the Runta service"
   homepage "https://github.com/runta-dev/runta"
-  version "0.0.4"
+  version "0.0.5"
 
   on_macos do
     on_arm do
       url "https://github.com/runta-dev/homebrew-tap/releases/download/v#{version}/runta-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "ef26094a0f00141a38f0ef83b55ededf61ef5a69d2f8345d51bd1eef87f5a74d"
+      sha256 "b35751cee8692b28e6aa5a3be3c315f00bbc6bb88896ada730c9b61773a5e1a8"
     end
     on_intel do
       odie "runta only supports Apple Silicon"
