f9c3975284465d754adbb6cbe66deaee7db27475
Xi Chen <chenxi.edu1997@gmail.com>
2026-07-15 21:30:46 +0000

    chore: release runta 0.1.13

--- Formula/runta.rb
+++ Formula/runta.rb
@@ -1,12 +1,12 @@
 class Runta < Formula
   desc "CLI for the Runta service"
   homepage "https://github.com/runta-dev/runta"
-  version "0.1.9"
+  version "0.1.13"
 
   on_macos do
     on_arm do
       url "https://github.com/runta-dev/homebrew-tap/releases/download/v#{version}/runta-#{version}-aarch64-apple-darwin.zip"
-      sha256 "8c3650d2324adfc316faba2b45ee507cca5e0bfcef1154f512174b3927f855d7"
+      sha256 "4819c1c9e5347414971497c4e038bec454d1f3616f142d5c662a0e511502799f"
     end
 
     on_intel do
