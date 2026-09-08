39f5a38fe65c6d612917d7eca3c7b9f2a1dd80f0
xieydd <xieydd@gmail.com>
2026-09-08 18:15:36 +0800

    chore: release runta 0.2.3 (#5)

--- Formula/runta.rb
+++ Formula/runta.rb
@@ -1,12 +1,12 @@
 class Runta < Formula
   desc "CLI for the Runta service"
   homepage "https://github.com/runta-dev/runta"
-  version "0.2.2"
+  version "0.2.3"
 
   on_macos do
     on_arm do
       url "https://github.com/runta-dev/homebrew-tap/releases/download/v#{version}/runta-#{version}-aarch64-apple-darwin.zip"
-      sha256 "47bcd627bc50c805cd4b103c696d520034f3ee1ff08830796516a5b067d1b6f4"
+      sha256 "14acad9db78d5594f98a1bddb896e2c952296b08cb8da0c78aed2e93143ea002"
     end
 
     on_intel do
